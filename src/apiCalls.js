74b738bc95540e569a6839adc85890be433a9ff4
Nicole Valentini <ncvalentini@gmail.com>
2022-04-26 09:28:03 -0600

    Refactor to functional component

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,3 +2,7 @@ export const getUrls = () => {
   return fetch('http://localhost:3001/api/v1/urls')
       .then(response => response.json())
 }
+
+export const postUrl = () => {
+  console.log('test to see if this will work')
+}
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,33 +1,54 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react'
 import './App.css';
-import { getUrls } from '../../apiCalls';
+import { getUrls, postUrl } from '../../apiCalls';
 import UrlContainer from '../UrlContainer/UrlContainer';
 import UrlForm from '../UrlForm/UrlForm';
 
-export class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      urls: []
-    }
-  }
 
-  componentDidMount() {
-  }
 
-  render() {
-    return (
-      <main className="App">
-        <header>
-          <h1>URL Shortener</h1>
-          <UrlForm />
-        </header>
+const App = () => {
+  const [urls, setUrls] = useState([])
 
-        <UrlContainer urls={this.state.urls}/>
-      </main>
-    );
-  }
+  return (
+    <main className="App">
+      <header>
+        <h1>URL Shortener</h1>
+        <UrlForm />
+      </header>
+
+      <UrlContainer urls={urls}/>
+    </main>
+  )
 }
+
+
+
+// export class App extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       urls: []
+//     }
+//   }
+//
+//   componentDidMount() {
+//     postUrl()
+//   }
+//
+  // render() {
+  //   return (
+  //     <main className="App">
+  //       <header>
+  //         <h1>URL Shortener</h1>
+  //         <UrlForm />
+  //       </header>
+  //
+  //       <UrlContainer urls={this.state.urls}/>
+  //     </main>
+  //   );
+  // }
+// }
 
 export default App;
 
