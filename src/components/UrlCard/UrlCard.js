f667fd6e62d2d16dc4aa3d21bb6b37700c395459
Nicole Valentini <ncvalentini@gmail.com>
2022-04-26 12:06:41 -0600

    Remove long url from card and dom display

--- src/components/UrlCard/UrlCard.js
+++ src/components/UrlCard/UrlCard.js
@@ -8,7 +8,6 @@ const UrlCard = ({id, long_url, short_url, title}) => {
       <h3 className='url-title'>{title}</h3>
       <p className='short-url-title'>Short Url:</p>
       <a className='short-url' href={short_url} target="blank">{short_url}</a>
-      <p className='long-url'>Long Url: {long_url}</p>
     </div>
   )
 }
